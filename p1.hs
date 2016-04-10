74581c5546132b74511714b324742883ac49efc5
Robert Doczi <robert.doczi@incquerylabs.com>
2016-04-10 13:56:41 +0200

    Solution for problem 1

--- p1.hs
+++ p1.hs
@@ -1,2 +1,11 @@
+
+divisibleBy :: Integer -> Integer -> Bool
+divisibleBy a b = mod a b == 0
+
+
 main :: IO ()
-main = do return ()
+main = do
+     let multipleOf3and5 = [x | x <- [1..999], divisibleBy x 3 || divisibleBy x  5]
+     print multipleOf3and5
+     print $ sum multipleOf3and5
+     return ()
